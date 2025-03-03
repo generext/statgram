faf65dc10d75a1da984988a8896cf315c2cbb398
Egor Kalinin <76444754+KaLiNkA-code@users.noreply.github.com>
2025-03-04 00:07:46 +0300

    udp schemas

--- statgram/schemas.py
+++ statgram/schemas.py
@@ -1,30 +1,5 @@
 from pydantic import BaseModel
-from typing import Union, List, Optional, Dict
-from aiogram.types import (
-    InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    ForceReply,
-    MessageEntity
-)
-
-class MessageSchema(BaseModel):
-    chat_id: Union[int, str]
-    text: str
-    parse_mode: Optional[str] = None
-    entities: Optional[List[MessageEntity]] = None
-    disable_web_page_preview: Optional[bool] = None
-    message_thread_id: Optional[int] = None
-    disable_notification: Optional[bool] = None
-    protect_content: Optional[bool] = None
-    reply_to_message_id: Optional[int] = None
-    allow_sending_without_reply: Optional[bool] = None
-    reply_markup: Optional[
-        Union[InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply]
-    ] = None
-
-    class Config:
-        arbitrary_types_allowed = True  # ✅ Разрешаем `aiogram.types.MessageEntity`
+from typing import Union, Optional, Dict
 
 class InitBotSchema(BaseModel):
     """
@@ -52,19 +27,10 @@ class ChatbotInfo(BaseModel):
     supports_inline_queries: Optional[bool] = None  # Поддерживает ли бот inline-режим
 
 class MessageSchema(BaseModel):
-    chat_id: Union[int, str]
-    text: str
+    chat_id: Union[int, str] = None
+    text: str = None
     parse_mode: Optional[str] = None
-    entities: Optional[List[MessageEntity]] = None
-    disable_web_page_preview: Optional[bool] = None
     message_thread_id: Optional[int] = None
-    disable_notification: Optional[bool] = None
-    protect_content: Optional[bool] = None
-    reply_to_message_id: Optional[int] = None
-    allow_sending_without_reply: Optional[bool] = None
-    reply_markup: Optional[
-        Union[InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply]
-    ] = None
 
     class Config:
         arbitrary_types_allowed = True
